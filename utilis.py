1929bd4e0f8c8becd3f9b07312f02927c76a6098
Genialdo <juniorstsc@gmail.com>
2022-04-24 01:17:36 -0300

    Incluso altenticação no banco e criado tabela de usuarios com as estencoes em requeriments

--- utilis.py
+++ utilis.py
@@ -1,4 +1,4 @@
-from models import Pessoas
+from models import Pessoas, Usuarios
 
 def insere_pessoas():
     pessoa = Pessoas(nome='Junior', idade=24)
@@ -28,8 +28,18 @@ def exclui_pessoa():
     pessoa = Pessoas.query.filter_by(nome='Junior').first()
     pessoa.delete()
 
+def insere_usuario(login, senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+def consulta_todos_usuarios():
+    usuarios = Usuarios.query.all()
+    print(usuarios)
+
 if __name__ == '__main__':
-    insere_pessoas()
-    altera_pessoa()
-    exclui_pessoa()
-    consulta_pessoas()
+    insere_usuario('junior', '123')
+    consulta_todos_usuarios()
+    #insere_pessoas()
+    #altera_pessoa()
+    #exclui_pessoa()
+    #consulta_pessoas()
